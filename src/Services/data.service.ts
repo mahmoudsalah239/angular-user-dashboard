d3f9ef61a9b06ece14ce7885e0573a154cec95b8
mahmoudsalah239 <mamosalah239@gmail.com>
2024-05-13 20:39:43 +0300

    Caching Implementation

--- src/Services/data.service.ts
+++ src/Services/data.service.ts
@@ -1,19 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { CashingService } from './cashing.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   private baseUrl = 'https://reqres.in/api/users';
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cachingService: CashingService
+  ) {}
 
   getUsers(page: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}?page=${page}`);
+    const cachedData = this.cachingService.get(`users-page-${page}`);
+    if (cachedData) {
+      return cachedData;
+    } else {
+      const request = this.http.get(`${this.baseUrl}?page=${page}`);
+      request.subscribe((response) => {
+        this.cachingService.set(`users-page-${page}`, response);
+      });
+      return request;
+    }
   }
 
   getUserDetails(id: number): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${id}`);
+    const cachedData = this.cachingService.get(`user-id-${id}`);
+    if (cachedData) {
+      return cachedData;
+    } else {
+      const request = this.http.get(`${this.baseUrl}/${id}`);
+      request.subscribe((response) => {
+        this.cachingService.set(`user-id-${id}`, response);
+      });
+
+      return request;
+    }
   }
 }
